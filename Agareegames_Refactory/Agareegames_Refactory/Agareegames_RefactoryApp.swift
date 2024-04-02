17e3988801a195f033841fc5966f1fcaf62b10b1
Benny <wonsang222@naver.com>
2024-04-02 21:13:19 +0900

    시작

---
+++ Agareegames_Refactory/Agareegames_Refactory/Agareegames_RefactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agareegames_RefactoryApp.swift
+//  Agareegames_Refactory
+//
+//  Created by 황원상 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Agareegames_RefactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Agareegames_Refactory/Agareegames_Refactory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Agareegames_Refactory
+//
+//  Created by 황원상 on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
